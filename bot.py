fe9de9a120a02640ff3aaf02b9c3ad0aea2a2707
hamidha5907110-rgb <hamidha5907110@gmail.com>
2026-09-13 11:34:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6006,7 +6006,7 @@ else:
     # ════════════════════════════════════════════════════════════════
     import os
     import sys
-    BOT_TOKEN         = "8889832400:AAHhsQcyLdFl4IqV2qCJfwlDN_HYPRL6N_4"  # ← replace with your bot token
+    BOT_TOKEN         = "8889832400:AAGU4AHlqmemoD3sOSpd3JLihRyxTJBQLqU"  # ← replace with your bot token
     OWNER_ID          = 2119464081             # ← replace with your Telegram ID
     SUPPORT_USERNAME  = "@fxrsale"
     MAX_USERBOTS      = int(os.environ.get("MAX_USERBOTS", "50") or "50")
